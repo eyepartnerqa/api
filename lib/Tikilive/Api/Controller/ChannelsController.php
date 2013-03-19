ad6e36da42ad3cce12834483943d66754e55d022
Adrian Videnie <avidenie@gmail.com>
2013-03-19 15:07:34 +0200

    Add Swagger annotations for generating API documentation

--- lib/Tikilive/Api/Controller/ChannelsController.php
+++ lib/Tikilive/Api/Controller/ChannelsController.php
@@ -2,6 +2,7 @@
 
 namespace Tikilive\Api\Controller;
 
+use Swagger\Annotations as SWG;
 use Tikilive\Application\ParameterContainer;
 use Tikilive\Controller\AbstractController;
 use Tikilive\Exception\Http\BadRequestException;
@@ -11,6 +12,11 @@ use Tikilive\Http\JsonResponse;
 
 /**
  * The API interface to Channel collection.
+ *
+ * @SWG\Resource(
+ *   apiVersion="1.0", swaggerVersion="1.1", resourcePath="/channels",
+ *   basePath="http://<api.example.org>/api/"
+ * )
  */
 class ChannelsController extends AbstractController
 {
@@ -35,6 +41,50 @@ class ChannelsController extends AbstractController
    * Returns all channels.
    *
    * @return array An array of channels.
+   *
+   * @SWG\Api(
+   *   path="/channels",
+   *   description="Operations on a specific channel",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="GET",
+   *       summary="Get all active channels",
+   *       notes="Returns informations about all active channels",
+   *       nickname="ChannelsController_getChannels",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="offset", description="The offset of the first row",
+   *           paramType="query", required="false", allowMultiple="false",
+   *           dataType="int", defaultValue="0"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="limit", description="The number of rows to return",
+   *           paramType="query", required="false", allowMultiple="false",
+   *           dataType="int", defaultValue="0",
+   *           @SWG\AllowableValues(valueType="RANGE", min="0", max="100")
+   *         ),
+   *         @SWG\Parameter(
+   *           name="order_by", description="Order by keyword",
+   *           paramType="query", required="false", allowMultiple="false",
+   *           dataType="string", defaultValue="id",
+   *           @SWG\AllowableValues(valueType="LIST", values="['id','name']")
+   *         ),
+   *         @SWG\Parameter(
+   *           name="direction", description="Order direction",
+   *           paramType="query", required="false", allowMultiple="false",
+   *           dataType="string", defaultValue="asc",
+   *           @SWG\AllowableValues(valueType="LIST", values="['ASC','DESC']")
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="400",
+   *           reason="Invalid parameters"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   protected function getChannels()
   {
@@ -85,6 +135,31 @@ class ChannelsController extends AbstractController
    *
    * @param int $id The channel ID.
    * @return array An associative array containing channel info.
+   *
+   * @SWG\Api(
+   *   path="/channels/{channelId}",
+   *   description="Operations on a specific channel",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="GET",
+   *       summary="Get info about a channel",
+   *       notes="Returns informations about a specific channel",
+   *       nickname="ChannelsController_getChannel",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="channelId", description="Channel ID to fetch",
+   *           paramType="path", required="true", allowMultiple="false", dataType="int"
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="404",
+   *           reason="Channel was not found or no longer available"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   protected function getChannel($id)
   {
@@ -122,6 +197,51 @@ class ChannelsController extends AbstractController
    * Creates a new channel.
    *
    * @param ParameterContainer $params Route parameters.
+   *
+   * @SWG\Api(
+   *   path="/channels",
+   *   description="Operations on a specific channel",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="POST",
+   *       summary="Create a new channel",
+   *       notes="Creates a new channel",
+   *       nickname="ChannelsController_postMethod",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="user_id", description="The parent user ID",
+   *           paramType="form", required="true", allowMultiple="false", dataType="int"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="name", description="The channel name",
+   *           paramType="form", required="true", allowMultiple="false", dataType="string"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="description", description="The channel description",
+   *           paramType="form", required="false", allowMultiple="false", dataType="string"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="status", description="The channel status",
+   *           paramType="form", required="false", allowMultiple="false",
+   *           dataType="string", defaultValue="enabled",
+   *           @SWG\AllowableValues(valueType="LIST", values="['enabled', 'disabled']")
+   *         ),
+   *         @SWG\Parameter(
+   *           name="published", description="The channel publishing status",
+   *           paramType="form", required="false", allowMultiple="false",
+   *           dataType="string", defaultValue="published",
+   *           @SWG\AllowableValues(valueType="LIST", values="['published', 'unpublished']")
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="400",
+   *           reason="Some fields did not pass validation"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   public function postMethod(ParameterContainer $params)
   {
@@ -158,6 +278,49 @@ class ChannelsController extends AbstractController
    * Updates an existing channel.
    *
    * @param ParameterContainer $params Route parameters.
+   *
+   * @SWG\Api(
+   *   path="/channels/{channelId}",
+   *   description="Operations on a specific channel",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="PUT",
+   *       summary="Update an existing channel",
+   *       notes="Updates an existing channel",
+   *       nickname="ChannelsController_putMethod",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="channelId", description="Channel ID to update",
+   *           paramType="path", required="true", allowMultiple="false", dataType="int"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="name", description="The channel name",
+   *           paramType="form", required="false", allowMultiple="false", dataType="string"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="description", description="The channel description",
+   *           paramType="form", required="false", allowMultiple="false", dataType="string"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="status", description="The channel status",
+   *           paramType="form", required="false", allowMultiple="false", dataType="string",
+   *           @SWG\AllowableValues(valueType="LIST", values="['enabled', 'disabled']")
+   *         ),
+   *         @SWG\Parameter(
+   *           name="published", description="The channel publishing status",
+   *           paramType="form", required="false", allowMultiple="false", dataType="string",
+   *           @SWG\AllowableValues(valueType="LIST", values="['published', 'unpublished']")
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="400",
+   *           reason="Some fields did not pass validation"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   public function putMethod(ParameterContainer $params)
   {
@@ -188,6 +351,31 @@ class ChannelsController extends AbstractController
    * Deletes an existing channel.
    *
    * @param ParameterContainer $params Route parameters.
+   *
+   * @SWG\Api(
+   *   path="/channels/{channelId}",
+   *   description="Operations on a specific channel",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="DELETE",
+   *       summary="Delete an existing channel",
+   *       notes="Deletes an existing channel",
+   *       nickname="ChannelsController_putMethod",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="channelId", description="Channel ID to delete",
+   *           paramType="path", required="true", allowMultiple="false", dataType="int"
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="404",
+   *           reason="Channel was not found"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   public function deleteMethod(ParameterContainer $params)
   {
--- lib/Tikilive/Api/Controller/UsersController.php
+++ lib/Tikilive/Api/Controller/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace Tikilive\Api\Controller;
 
+use Swagger\Annotations as SWG;
 use Tikilive\Application\ParameterContainer;
 use Tikilive\Controller\AbstractController;
 use Tikilive\Exception\Http\BadRequestException;
@@ -11,6 +12,11 @@ use Tikilive\Http\JsonResponse;
 
 /**
  * The API interface to User collection.
+ *
+ * @SWG\Resource(
+ *   apiVersion="1.0", swaggerVersion="1.1", resourcePath="/users",
+ *   basePath="http://<api.example.org>/api/"
+ * )
  */
 class UsersController extends AbstractController
 {
@@ -35,6 +41,50 @@ class UsersController extends AbstractController
    * Returns all users.
    *
    * @return array An array of users.
+   *
+   * @SWG\Api(
+   *   path="/users",
+   *   description="Operations on a specific user",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="GET",
+   *       summary="Get all active users",
+   *       notes="Returns informations about all active users",
+   *       nickname="UsersController_getUsers",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="offset", description="The offset of the first row",
+   *           paramType="query", required="false", allowMultiple="false",
+   *           dataType="int", defaultValue="0"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="limit", description="The number of rows to return",
+   *           paramType="query", required="false", allowMultiple="false",
+   *           dataType="int", defaultValue="0",
+   *           @SWG\AllowableValues(valueType="RANGE", min="0", max="100")
+   *         ),
+   *         @SWG\Parameter(
+   *           name="order_by", description="Order by keyword",
+   *           paramType="query", required="false", allowMultiple="false",
+   *           dataType="string", defaultValue="id",
+   *           @SWG\AllowableValues(valueType="LIST", values="['id','username']")
+   *         ),
+   *         @SWG\Parameter(
+   *           name="direction", description="Order direction",
+   *           paramType="query", required="false", allowMultiple="false",
+   *           dataType="string", defaultValue="asc",
+   *           @SWG\AllowableValues(valueType="LIST", values="['ASC','DESC']")
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="400",
+   *           reason="Invalid parameters"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   protected function getUsers()
   {
@@ -79,6 +129,31 @@ class UsersController extends AbstractController
    *
    * @param int $id The user ID.
    * @return array An associative array containing user info.
+   *
+   * @SWG\Api(
+   *   path="/users/{userId}",
+   *   description="Operations on a specific user",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="GET",
+   *       summary="Get info about a user",
+   *       notes="Returns informations about a specific user",
+   *       nickname="UsersController_getUser",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="userId", description="User ID to fetch",
+   *           paramType="path", required="true", allowMultiple="false", dataType="int"
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="404",
+   *           reason="User was not found or the profile is no longer available"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   protected function getUser($id)
   {
@@ -105,6 +180,42 @@ class UsersController extends AbstractController
    * Creates a new user.
    *
    * @param ParameterContainer $params Route parameters.
+   * @return array An associative array containing the generated user ID.
+   *
+   * @SWG\Api(
+   *   path="/users",
+   *   description="Operations on a specific user",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="POST",
+   *       summary="Create a new user",
+   *       notes="Creates a new user",
+   *       nickname="UsersController_postMethod",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="username", description="The username",
+   *           paramType="form", required="true", allowMultiple="false", dataType="string"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="email", description="The email address of the user",
+   *           paramType="form", required="true", allowMultiple="false", dataType="string"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="status", description="The user status",
+   *           paramType="form", required="false", allowMultiple="false",
+   *           dataType="string", defaultValue="enabled",
+   *           @SWG\AllowableValues(valueType="LIST", values="['enabled', 'disabled']")
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="400",
+   *           reason="Some fields did not pass validation"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   public function postMethod(ParameterContainer $params)
   {
@@ -133,6 +244,44 @@ class UsersController extends AbstractController
    * Updates an existing user.
    *
    * @param ParameterContainer $params Route parameters.
+   *
+   * @SWG\Api(
+   *   path="/users/{userId}",
+   *   description="Operations on a specific user",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="PUT",
+   *       summary="Update an existing user",
+   *       notes="Updates an existing user",
+   *       nickname="UsersController_putMethod",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="userId", description="User ID to update",
+   *           paramType="path", required="true", allowMultiple="false", dataType="int"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="username", description="The username",
+   *           paramType="form", required="false", allowMultiple="false", dataType="string"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="email", description="The email address of the user",
+   *           paramType="form", required="false", allowMultiple="false", dataType="string"
+   *         ),
+   *         @SWG\Parameter(
+   *           name="status", description="The user status",
+   *           paramType="form", required="false", allowMultiple="false", dataType="string",
+   *           @SWG\AllowableValues(valueType="LIST", values="['enabled', 'disabled']")
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="400",
+   *           reason="Some fields did not pass validation"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   public function putMethod(ParameterContainer $params)
   {
@@ -140,7 +289,7 @@ class UsersController extends AbstractController
     $user = $userModel->findById($params->get('id'));
 
     if ($user === null) {
-      throw new NotFoundException('User does not exist.');
+      throw new NotFoundException('User was not found.');
     }
 
     $request = $this->get('request');
@@ -162,6 +311,31 @@ class UsersController extends AbstractController
    * Deletes an existing user.
    *
    * @param ParameterContainer $params Route parameters.
+   *
+   * @SWG\Api(
+   *   path="/users/{userId}",
+   *   description="Operations on a specific user",
+   *   @SWG\Operations(
+   *     @SWG\Operation(
+   *       httpMethod="DELETE",
+   *       summary="Delete an existing user",
+   *       notes="Deletes an existing user",
+   *       nickname="UsersController_putMethod",
+   *       @SWG\Parameters(
+   *         @SWG\Parameter(
+   *           name="userId", description="User ID to delete",
+   *           paramType="path", required="true", allowMultiple="false", dataType="int"
+   *         )
+   *       ),
+   *       @SWG\ErrorResponses(
+   *         @SWG\ErrorResponse(
+   *           code="404",
+   *           reason="User was not found"
+   *         )
+   *       )
+   *     )
+   *   )
+   * )
    */
   public function deleteMethod(ParameterContainer $params)
   {
@@ -169,7 +343,7 @@ class UsersController extends AbstractController
     $user = $userModel->findById($params->get('id'));
 
     if ($user === null) {
-      throw new NotFoundException('User does not exist.');
+      throw new NotFoundException('User was not found.');
     }
 
     $userModel->delete($user->getId());
